e0b526a0d1f86d19202a21a643d8f308e62194ee
Grzegorz Poreba <grzegorz.poreba.73@gmail.com>
2020-03-28 23:00:58 +0100

    Updates

---
+++ precache-manifest.2b82c09504ba6f5d0f42c003790f5cbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab498a450a255332a746b3bb3008e6e4",
+    "url": "/Porfolio/index.html"
+  },
+  {
+    "revision": "e7d7b14cedc3054d8f67",
+    "url": "/Porfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "18311cb097a6ae631286",
+    "url": "/Porfolio/static/css/main.f57ff103.chunk.css"
+  },
+  {
+    "revision": "e7d7b14cedc3054d8f67",
+    "url": "/Porfolio/static/js/2.ef9c557a.chunk.js"
+  },
+  {
+    "revision": "9903440051e26cc6d85d7de45bb7193b",
+    "url": "/Porfolio/static/js/2.ef9c557a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18311cb097a6ae631286",
+    "url": "/Porfolio/static/js/main.6d412c77.chunk.js"
+  },
+  {
+    "revision": "f87a9fea9846cb78aa00",
+    "url": "/Porfolio/static/js/runtime-main.d177a70d.js"
+  },
+  {
+    "revision": "14e6fd0674db9b7ac928270fd51c5495",
+    "url": "/Porfolio/static/media/bootstrapIT.14e6fd06.PNG"
+  },
+  {
+    "revision": "6ec7318451fda601d235aa7ef0df5f8d",
+    "url": "/Porfolio/static/media/gotowaniev1.6ec73184.PNG"
+  },
+  {
+    "revision": "085a5a9b4b3d1b2f8efe1a83417f6b29",
+    "url": "/Porfolio/static/media/landscape.085a5a9b.jpg"
+  }
+]);
